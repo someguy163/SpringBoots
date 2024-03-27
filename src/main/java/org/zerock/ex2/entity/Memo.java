1c8f44960d6b7d4450579a41507da0e5b30a421d
someguy163 <wnguddl163@naver.com>
2024-03-27 10:37:41 +0900

    2024-03-27

---
+++ src/main/java/org/zerock/ex2/entity/Memo.java
@@ -0,0 +1,32 @@
+package org.zerock.ex2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+//table 네임
+@Table(name = "tbl_memo")
+//
+@Getter
+@Setter
+
+
+//
+//builer는 객체생성가능하게
+@Builder
+//아래 두개는 @Builder를 사용할때 무조건 사용해야한다
+@AllArgsConstructor
+@NoArgsConstructor
+//
+
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+
+
+}
